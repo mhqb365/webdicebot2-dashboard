812a311ceab9471de7fd05ae375b4a88b4c76173
mhqb365 <mhqb365@gmail.com>
2021-01-26 00:27:13 +0700

    new-deploy

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,8 @@
       <div class="col-9">
         <router-view />
       </div>
+
+      <a class="to-top" href="#app">Go top</a>
     </div>
   </div>
 </template>
@@ -20,11 +22,48 @@ export default {
   components: {
     Left,
   },
+  mounted: function () {
+    var duration = 500;
+    $(window).scroll(function () {
+      if (jQuery(this).scrollTop() > 200) {
+        jQuery(".to-top").fadeIn(duration);
+      } else {
+        jQuery(".to-top").fadeOut(duration);
+      }
+    });
+
+    $(".to-top").click(function (event) {
+      event.preventDefault();
+      jQuery("html").animate({ scrollTop: 0 }, duration);
+      return false;
+    });
+  },
 };
 </script>
 
 <style>
 #app {
-  background: url("/static/bg.png") no-repeat;
+  background: url("/static/bg.png") no-repeat top center;
+}
+
+.to-top {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  text-decoration: none;
+  color: #fff;
+  background: #007bff;
+  padding: 10px 20px;
+  display: none;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+}
+
+.to-top:hover {
+  text-decoration: none;
+  color: #fff;
+  background: #0069d9;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
 }
 </style>
