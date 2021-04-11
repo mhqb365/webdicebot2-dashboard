2d6d5d02ce1fe086d238cf989c91b3e68f2a1fa8
mhqb365 <mhqb365@gmail.com>
2021-04-11 18:36:48 +0700

    new-deploy

--- src/pages/MyLicense.vue
+++ src/pages/MyLicense.vue
@@ -1,97 +1,129 @@
 <template>
-  <div class="pb-5">
-    <h2 class="text-primary">My license</h2>
+  <div>
+    <div class="pb-5">
+      <h2 class="text-primary">Summary</h2>
 
-    <p>Total: {{ totalDocs }} | Pages: {{ totalPages }}</p>
+      <div class="row">
+        <div class="col-6">
+          <div class="card p-4">
+            Total license
+            <br />
+            <h4 class="text-primary">
+              <div v-if="isLoading2" class="spinner-border text-muted"></div>
+              <span v-else> {{ total }}</span>
+            </h4>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="card p-4">
+            TRX spend
+            <br />
+            <h4 class="text-success">
+              <div v-if="isLoading2" class="spinner-border text-muted"></div>
+              <span v-else> {{ Number(spendTrx).toFixed(6) }}</span>
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
 
-    <ul class="pagination">
-      <li v-if="hasPrevPage" class="page-item">
-        <button type="button" class="page-link" @click="myLicense(page - 1)">
-          Previous
-        </button>
-      </li>
-      <li class="page-item active">
-        <button type="button" class="page-link">{{ page }}</button>
-      </li>
-      <li v-if="hasNextPage" class="page-item">
-        <button type="button" class="page-link" @click="myLicense(page + 1)">
-          Next
-        </button>
-      </li>
-    </ul>
+    <div class="pb-5">
+      <h2 class="text-primary">My license</h2>
 
-    <p class="small">
-      <span class="text-success">Green</span>: Paid and working |
-      <span class="text-danger">Red</span>: Expired |
-      <span class="text-warning">Yellow</span>: Locked
-    </p>
+      <p>Total: {{ totalDocs }} | Pages: {{ totalPages }}</p>
 
-    <div class="table-responsive-sm">
-      <div v-if="isLoading" class="spinner-border text-muted"></div>
+      <ul class="pagination">
+        <li v-if="hasPrevPage" class="page-item">
+          <button type="button" class="page-link" @click="myLicense(page - 1)">
+            Previous
+          </button>
+        </li>
+        <li class="page-item active">
+          <button type="button" class="page-link">{{ page }}</button>
+        </li>
+        <li v-if="hasNextPage" class="page-item">
+          <button type="button" class="page-link" @click="myLicense(page + 1)">
+            Next
+          </button>
+        </li>
+      </ul>
 
-      <table v-else class="table table-bordered table-hover bg-white table-sm">
-        <thead>
-          <tr>
-            <th>Price</th>
-            <th>License</th>
-            <th>Begin</th>
-            <th>End</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="doc in docs"
-            :key="doc._id"
-            v-bind:class="[
-              (Date.now() - new Date(doc.time)) / 864e5 > doc.limit
-                ? 'text-danger'
-                : doc.locked
-                ? 'text-warning'
-                : doc.type == 'Pay'
-                ? 'text-success'
-                : 'text-dark',
-            ]"
-          >
-            <td>
-              {{
-                Number(doc.price) > 0 ? Number(doc.price).toFixed(6) : "Free"
-              }}
-            </td>
-            <td>
-              <div class="input-group mb-3">
-                <input type="text" class="form-control" :value="doc.value" />
-                <div class="input-group-append">
-                  <button
-                    type="button"
-                    class="btn btn-primary"
-                    v-clipboard="() => doc.value"
-                    v-clipboard:success="clipboardSuccess"
-                    v-clipboard:error="clipboardError"
-                  >
-                    Copy
-                  </button>
+      <p class="small">
+        <span class="text-success">Green</span>: Paid and working |
+        <span class="text-danger">Red</span>: Expired |
+        <span class="text-warning">Yellow</span>: Locked
+      </p>
+
+      <div class="table-responsive-sm">
+        <div v-if="isLoading" class="spinner-border text-muted"></div>
+
+        <table
+          v-else
+          class="table table-bordered table-hover bg-white table-sm"
+        >
+          <thead>
+            <tr>
+              <th>Price</th>
+              <th>License</th>
+              <th>Begin</th>
+              <th>End</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="doc in docs"
+              :key="doc._id"
+              v-bind:class="[
+                (Date.now() - new Date(doc.time)) / 864e5 > doc.limit
+                  ? 'text-danger'
+                  : doc.locked
+                  ? 'text-warning'
+                  : doc.type == 'Pay'
+                  ? 'text-success'
+                  : 'text-dark',
+              ]"
+            >
+              <td>
+                {{
+                  Number(doc.price) > 0 ? Number(doc.price).toFixed(6) : "Free"
+                }}
+              </td>
+              <td>
+                <div class="input-group mb-3">
+                  <input type="text" class="form-control" :value="doc.value" />
+                  <div class="input-group-append">
+                    <button
+                      type="button"
+                      class="btn btn-primary"
+                      v-clipboard="() => doc.value"
+                      v-clipboard:success="clipboardSuccess"
+                      v-clipboard:error="clipboardError"
+                    >
+                      Copy
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </td>
-            <td>
-              {{
-                new Date(doc.time).toLocaleString("en-GB", {
-                  timeZone: "UTC",
-                })
-              }}
-            </td>
-            <td>
-              {{
-                new Date(
-                  Number(864e5 * doc.limit) + Number(new Date(doc.time))
-                ).toLocaleString("en-GB", {
-                  timeZone: "UTC",
-                })
-              }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
+              </td>
+              <td>
+                {{
+                  new Date(doc.time).toLocaleString("en-GB", {
+                    timeZone: "UTC",
+                  })
+                }}
+              </td>
+              <td>
+                {{
+                  new Date(
+                    Number(864e5 * doc.limit) + Number(new Date(doc.time))
+                  ).toLocaleString("en-GB", {
+                    timeZone: "UTC",
+                  })
+                }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -110,10 +142,14 @@ export default {
       totalPages: 0,
       hasPrevPage: false,
       hasNextPage: false,
+      isLoading2: false,
+      total: 0,
+      spendTrx: 0,
     };
   },
   mounted: function () {
     this.myLicense(this.page);
+    this.getSummary();
   },
   methods: {
     myLicense: function (page) {
@@ -143,6 +179,30 @@ export default {
         })
         .catch((error) => {
           this.isLoading = false;
+          this.showAlert(error.response.data, false);
+          window.location.href = "/Logout";
+        });
+    },
+    getSummary: function () {
+      this.isLoading2 = true;
+      axios({
+        url: API_URL + "/license/mySummary/" + localStorage.getItem("userName"),
+        method: "GET",
+        headers: {
+          Auth: localStorage.getItem("token"),
+        },
+      })
+        .then((response) => {
+          this.isLoading2 = false;
+          let res = response.data;
+          // console.log(res);
+          res.map((r) => {
+            this.total++;
+            this.spendTrx += r.price;
+          });
+        })
+        .catch((error) => {
+          this.isLoading2 = false;
           this.showAlert(error.response.data, false);
           window.location.href = "/Logout";
         });
