46fbc4e95ac45cb5e6adf2cbf6b109f03140de03
mhqb365 <mhqb365@gmail.com>
2021-06-09 12:15:42 +0700

    new-deploy

--- src/pages/BotInstaller.vue
+++ src/pages/BotInstaller.vue
@@ -1,38 +1,99 @@
 <template>
-  <div class="pb-5">
-    <h2 class="text-primary">Bot installer</h2>
+  <div>
+    <div class="pb-5">
+      <h2 class="text-primary">Bot installer</h2>
 
-    <p class="small">
-      + Only use the installer at dashboard.webdicebot.xyz if you do not want
-      someone fake us to trick you
-      <br />
-      + With luckyfish casino, you must disable HOTKEYS can put dice script to
-      script box
-      <br />
-      + With Lua mode, dice script must follow syntax of Lua version 5.3. If
-      have any syntax error bot will not work
-    </p>
+      <p class="small">
+        + Only use the installer at dashboard.webdicebot.xyz if you do not want
+        someone fake us to trick you
+        <br />
+        + With luckyfish casino, you must disable HOTKEYS can put dice script to
+        script box
+        <br />
+        + With Lua mode, dice script must follow syntax of Lua version 5.3. If
+        have any syntax error bot will not work
+      </p>
 
-    <div class="form-group">
-      <select class="form-control" v-model="selectName" @change="changeBot">
-        <option v-for="bot in bots" :key="bot.name" :value="bot.name">
-          {{ bot.name }}
-        </option>
-      </select>
+      <div class="form-group">
+        <select class="form-control" v-model="selectName" @change="changeBot">
+          <option v-for="bot in bots" :key="bot.name" :value="bot.name">
+            {{ bot.name }}
+          </option>
+        </select>
+      </div>
+
+      <div class="form-group">
+        <textarea class="form-control" rows="5" v-model="selectValue">
+        </textarea>
+      </div>
+
+      <button
+        class="btn btn-primary btn-block"
+        v-clipboard="() => selectValue"
+        v-clipboard:success="clipboardSuccess"
+        v-clipboard:error="clipboardError"
+      >
+        Copy
+      </button>
     </div>
 
-    <div class="form-group">
-      <textarea class="form-control" rows="5" v-model="selectValue"> </textarea>
-    </div>
+    <div class="pb-5">
+      <h2 class="text-primary">Emulator</h2>
+      <p class="small">
+        + Emulator is a casino simulation software run on your computer. Use it
+        for install Web DiceBot for that casino
+      </p>
 
-    <button
-      class="btn btn-primary btn-block"
-      v-clipboard="() => selectValue"
-      v-clipboard:success="clipboardSuccess"
-      v-clipboard:error="clipboardError"
-    >
-      Copy
-    </button>
+      <div class="">
+        <a
+          href="https://github.com/mhqb365/webdicebot-emulator"
+          target="_blank"
+          class="btn btn-primary"
+          >Download</a
+        >
+      </div>
+    </div>
+    <div class="pb-5">
+      <h2 class="text-primary">How to use?</h2>
+
+      <h5>Windows</h5>
+      <ul class="list-group mb-3">
+        <li class="list-group-item">
+          Step 1: Double Click webdicebot-win.exe File, or Open Command Prompt at emulator you need
+        </li>
+        <li class="list-group-item">
+          Step 2: Open browser then go to url show in Command Prompt
+        </li>
+      </ul>
+
+      <h4>Mac</h4>
+      <ul class="list-group mb-3">
+        <li class="list-group-item">
+          Step 1: Open Terminal in folder emulator you need then paste in order the 2 lines below
+        </li>
+        <pre class="bg-white shadow-sm">
+chmod +x webdicebot-macos
+./webdicebot-macos</pre
+        >
+        <li class="list-group-item">
+          Step 2: Open browser then go to url show in Terminal
+        </li>
+      </ul>
+
+      <h4>Linux</h4>
+      <ul class="list-group mb-3">
+        <li class="list-group-item">
+          Step 1: Open Terminal in folder emulator you need then Paste in order the 2 lines below
+        </li>
+        <pre class="bg-white shadow-sm">
+chmod +x webdicebot-linux
+./webdicebot-linux</pre
+        >
+        <li class="list-group-item">
+          Step 2: Open browser then go to url show in Terminal
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -172,6 +233,14 @@ script.src = BOT_URL+'/bot/init/freebitco'
 document.body.appendChild(script)`,
         },
         {
+          name: "jacksclub.io - dice (work on emulator)",
+          value: `const BOT_URL = '${API_URL}'
+var script = document.createElement('script')
+script.type = 'text/javascript'
+script.src = BOT_URL+'/bot/init/jacksclub.io-dice'
+document.body.appendChild(script)`,
+        },
+        {
           name: "luckyfish.io - dice classic",
           value: `const BOT_URL = '${API_URL}'
 var script = document.createElement('script')
@@ -235,14 +304,14 @@ script.type = 'text/javascript'
 script.src = BOT_URL+'/bot/init/stake-limbo'
 document.body.appendChild(script)`,
         },
-//         {
-//           name: "stargames.bz - dice",
-//           value: `const BOT_URL = '${API_URL}'
-// var script = document.createElement('script')
-// script.type = 'text/javascript'
-// script.src = BOT_URL+'/bot/init/stargames.bz-dice'
-// document.body.appendChild(script)`,
-//         },
+        //         {
+        //           name: "stargames.bz - dice",
+        //           value: `const BOT_URL = '${API_URL}'
+        // var script = document.createElement('script')
+        // script.type = 'text/javascript'
+        // script.src = BOT_URL+'/bot/init/stargames.bz-dice'
+        // document.body.appendChild(script)`,
+        //         },
         {
           name: "trustdice.win - dice",
           value: `const BOT_URL = '${API_URL}'
