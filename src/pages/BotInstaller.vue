7c4f6acdf88feb2081847721f6bbc893f2948edf
mhqb365 <mhqb365@gmail.com>
2021-06-09 17:13:17 +0700

    new-deploy

--- src/pages/BotInstaller.vue
+++ src/pages/BotInstaller.vue
@@ -40,20 +40,22 @@
     <div class="pb-5">
       <h2 class="text-primary">Emulator</h2>
       <p class="small">
-        + Emulator is a casino simulation software run on your computer. Use it
-        for install Web DiceBot for that casino
+        + Emulator is a dice sites simulation. It will help Web DiceBot work
+        with dice sites
       </p>
 
-      <div class="">
-        <a
-          href="https://github.com/mhqb365/webdicebot-emulator"
-          target="_blank"
-          class="btn btn-primary"
-          >Download</a
-        >
-      </div>
+      <ul class="list-group">
+        <li class="list-group-item">
+          <a
+            href="https://jacksclub-dice-emulator.webdicebot.xyz/"
+            target="_blank"
+          >
+            JacksClub.io - Dice
+          </a>
+        </li>
+      </ul>
     </div>
-    <div class="pb-5">
+    <!-- <div class="pb-5">
       <h2 class="text-primary">How to use?</h2>
 
       <div class="text-center">
@@ -62,48 +64,7 @@
           Your browser does not support HTML video
         </video>
       </div>
-
-      <h5>Windows</h5>
-      <ul class="list-group mb-3">
-        <li class="list-group-item">
-          Step 1: Double Click webdicebot-win.exe File, or Open Command Prompt
-          at emulator you need
-        </li>
-        <li class="list-group-item">
-          Step 2: Open browser then go to url show in Command Prompt
-        </li>
-      </ul>
-
-      <h4>Mac</h4>
-      <ul class="list-group mb-3">
-        <li class="list-group-item">
-          Step 1: Open Terminal in folder emulator you need then paste in order
-          the 2 lines below
-        </li>
-        <pre class="bg-white shadow-sm">
-chmod +x webdicebot-macos
-./webdicebot-macos</pre
-        >
-        <li class="list-group-item">
-          Step 2: Open browser then go to url show in Terminal
-        </li>
-      </ul>
-
-      <h4>Linux</h4>
-      <ul class="list-group mb-3">
-        <li class="list-group-item">
-          Step 1: Open Terminal in folder emulator you need then Paste in order
-          the 2 lines below
-        </li>
-        <pre class="bg-white shadow-sm">
-chmod +x webdicebot-linux
-./webdicebot-linux</pre
-        >
-        <li class="list-group-item">
-          Step 2: Open browser then go to url show in Terminal
-        </li>
-      </ul>
-    </div>
+    </div> -->
   </div>
 </template>
 
